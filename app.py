0ca7263ad3d273b8964eb242725d6a74ef0aac3f
alithaheemzeeshan-blip <alithaheemzeeshan@gmail.com>
2026-09-02 21:40:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 # 1. Page Config
 st.set_page_config(page_title="AI Agent for Detection", page_icon="🚀", layout="wide")
 
-st.title("🐾 AI Agent for Detection")
+st.title("🚀 AI Agent for Detection")
 st.write("Created By Zeeshan Thaheem")
 
 # 2. Load custom animal model (best.pt)
